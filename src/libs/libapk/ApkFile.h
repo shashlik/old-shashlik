2da6d0d4a3092d46fd09cdad35037d4633d65744
Inge Wallin <inge@lysator.liu.se>
2014-07-11 11:29:38 +0700

    Make libapk compile and link correctly with the dependencies.

---
+++ src/libs/libapk/ApkFile.h
@@ -0,0 +1,58 @@
+/* This file is part of Shashlik
+
+   Copyright (C) 2014 Inge Wallin <inge@lysator.liu.se>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#ifndef APKFILE_H
+#define APKFILE_H
+
+
+// Qt
+#include <QHash>
+
+// KDE Frameworks
+#include <KArchive>
+
+//
+//#include "FileCollector.h"
+
+
+class QString;
+class QByteArray;
+
+class ApkFile // : public FileCollector
+{
+public:
+    ApkFile();
+    ~ApkFile();
+
+    // When you have created all the content and added it using
+    // addContentFile(), call this function once and it will write the
+    // apk to the disk.
+    bool writeApk(const QString &fileName,
+		  const QByteArray &appIdentification,
+		  QHash<QString, QString> metadata);
+
+private:
+// Private functions here
+
+private:
+// Private data members here
+};
+
+#endif // APKFILE_H
