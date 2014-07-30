6842efc3d874113dd8c7637fc9ed8c55df4ef858
Inge Wallin <inge@lysator.liu.se>
2014-07-30 17:46:45 +0700

    Also remove shashlikversion.h when cleaning up

--- build/Clean.sh
+++ build/Clean.sh
@@ -2,4 +2,4 @@
 
 # As a simple way to lessen the risk of invoking this in the wrong place:
 # Don't invoke rm -r unless the previous rm's work.
-rm -r CMakeFiles CMakeCache.txt && rm *.cmake Makefile && rm -r src
+rm -r CMakeFiles CMakeCache.txt && rm *.cmake Makefile && rm -rf src shashlikversion.h
