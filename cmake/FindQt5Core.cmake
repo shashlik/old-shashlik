d92a4e794e1ecef3d3f341ab9bbe4f8cea45cc25
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-02-10 13:59:21 +0000

    Add some new find modules
    One of these will look very odd, but it all comes down to 32bit: Qt5's
    cmake modules don't support finding the 32 bit libraries on a 64 bit
    system, so we kind of have to hack our way out of it. It could very
    possibly be made more pleasant by standing on top of the other bits
    of the Qt5 system, but right now this is it...

---
+++ cmake/FindQt5Core.cmake
@@ -0,0 +1,32 @@
+# WARNING This is hack territory. The reason this exists at all is that the highly powerful and elaborate
+# Qt5 cmake modules are incapable of providing 32bit libraries on a 64bit system. So, we do this. Not exactly
+# an optimal situation, but oh well.
+
+if (QT5CORE32_INCLUDE_DIRS AND QT5CORE32_LIBRARIES)
+    # Already in cache, be silent
+    set(QT5CORE32_FIND_QUIETLY TRUE)
+endif (QT5CORE32_INCLUDE_DIRS AND QT5CORE32_LIBRARIES)
+
+find_path(QT5CORE32_INCLUDE_DIR NAMES qglobal.h
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/QtCore
+    /usr/include/qt5/QtCore
+    )
+find_path(QT5CORE32_BASE_INCLUDE_DIR NAMES QtCore/qglobal.h
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/qt5
+    )
+find_library(QT5CORE32_LIBRARIES NAMES Qt5Core libQt5Core PATHS /usr/lib)
+SET(QT5CORE32_INCLUDE_DIRS "${QT5CORE32_INCLUDE_DIR};${QT5CORE32_BASE_INCLUDE_DIR}")
+
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(QT5CORE DEFAULT_MSG QT5CORE32_INCLUDE_DIRS QT5CORE32_LIBRARIES)
+
+mark_as_advanced(QT5CORE32_INCLUDE_DIRS QT5CORE32_LIBRARIES)
