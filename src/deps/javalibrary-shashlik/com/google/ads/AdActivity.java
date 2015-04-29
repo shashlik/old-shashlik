dc9056e874b948827e2fc9a7dc37a7d7626fa4f0
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-04-29 14:19:54 +0100

    Add a skeleton activity (non-functional for now)

---
+++ src/deps/javalibrary-shashlik/com/google/ads/AdActivity.java
@@ -0,0 +1,37 @@
+package com.google.ads;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+ 
+public class AdActivity extends Activity {
+@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // The activity is being created.
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+}
