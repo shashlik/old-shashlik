e9b83353335ec40ecb86c1bb9c05f0d950cf6b5e
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-06-09 18:53:36 +0100

    Add find modules for Qt5 Gui, Widgets and X11Extras

---
+++ cmake/FindQt5Gui.cmake
@@ -0,0 +1,32 @@
+# WARNING This is hack territory. The reason this exists at all is that the highly powerful and elaborate
+# Qt5 cmake modules are incapable of providing 32bit libraries on a 64bit system. So, we do this. Not exactly
+# an optimal situation, but oh well.
+
+if (QT5GUI32_INCLUDE_DIRS AND QT5GUI32_LIBRARIES)
+    # Already in cache, be silent
+    set(QT5GUI32_FIND_QUIETLY TRUE)
+endif (QT5GUI32_INCLUDE_DIRS AND QT5GUI32_LIBRARIES)
+
+find_path(QT5GUI32_INCLUDE_DIR NAMES QtGuiVersion
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/QtGui
+    /usr/include/qt5/QtGui
+    )
+find_path(QT5GUI32_BASE_INCLUDE_DIR NAMES QtGui/QtGuiVersion
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/qt5
+    )
+find_library(QT5GUI32_LIBRARIES NAMES Qt5Gui libQt5Gui PATHS /usr/lib)
+SET(QT5GUI32_INCLUDE_DIRS "${QT5GUI32_INCLUDE_DIR};${QT5GUI32_BASE_INCLUDE_DIR}")
+
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(QT5GUI DEFAULT_MSG QT5GUI32_INCLUDE_DIRS QT5GUI32_LIBRARIES)
+
+mark_as_advanced(QT5GUI32_INCLUDE_DIRS QT5GUI32_LIBRARIES)
---
+++ cmake/FindQt5Widgets.cmake
@@ -0,0 +1,32 @@
+# WARNING This is hack territory. The reason this exists at all is that the highly powerful and elaborate
+# Qt5 cmake modules are incapable of providing 32bit libraries on a 64bit system. So, we do this. Not exactly
+# an optimal situation, but oh well.
+
+if (QT5WIDGETS32_INCLUDE_DIRS AND QT5WIDGETS32_LIBRARIES)
+    # Already in cache, be silent
+    set(QT5WIDGETS32_FIND_QUIETLY TRUE)
+endif (QT5WIDGETS32_INCLUDE_DIRS AND QT5WIDGETS32_LIBRARIES)
+
+find_path(QT5WIDGETS32_INCLUDE_DIR NAMES QtWidgetsVersion
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/QtWidgets
+    /usr/include/qt5/QtWidgets
+    )
+find_path(QT5WIDGETS32_BASE_INCLUDE_DIR NAMES QtWidgets/QtWidgetsVersion
+    PATHS
+    ${CMAKE_INSTALL_PREFIX}/include
+    ${KDE4_INCLUDE_DIR}
+    /usr/include
+    /usr/include/qt5
+    )
+find_library(QT5WIDGETS32_LIBRARIES NAMES Qt5Widgets libQt5Widgets PATHS /usr/lib)
+SET(QT5WIDGETS32_INCLUDE_DIRS "${QT5WIDGETS32_INCLUDE_DIR};${QT5WIDGETS32_BASE_INCLUDE_DIR}")
+
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(QT5WIDGETS DEFAULT_MSG QT5WIDGETS32_INCLUDE_DIRS QT5WIDGETS32_LIBRARIES)
+
+mark_as_advanced(QT5WIDGETS32_INCLUDE_DIRS QT5WIDGETS32_LIBRARIES)
