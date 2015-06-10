bee84779c0bb7c09cb8ca91ad7061f54a42a0653
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-06-10 22:01:10 +0100

    Don't need this silly thing... old stuff

--- build/Clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# As a simple way to lessen the risk of invoking this in the wrong place:
-# Don't invoke rm -r unless the previous rm's work.
-rm -r CMakeFiles CMakeCache.txt && rm *.cmake Makefile && rm -rf src shashlikversion.h
