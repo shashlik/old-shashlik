9cd485e1dc7736f10a46b6fa6ac9ab92a28fc57e
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-06-10 22:22:11 +0100

    Handle the include location for libc stuff on ubuntu etc

--- src/deps/libbinder/sys/types.h
+++ src/deps/libbinder/sys/types.h
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <sys/syscall.h>
+#ifdef HAS_SPECIAL_INCLUDE_DIRS
+#include <i386-linux-gnu/sys/types.h>
+#else
 #include <../include/sys/types.h>
+#endif
 #include <signal.h>
 
 #define gettid() syscall(SYS_gettid)
