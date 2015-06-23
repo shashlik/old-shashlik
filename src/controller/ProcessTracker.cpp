229e53b59bf39f5cd5932d33d4852440d4523201
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-06-23 19:51:19 +0100

    Add a process tracking class (very simple for now) to the controller

---
+++ src/controller/ProcessTracker.cpp
@@ -0,0 +1,104 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2015  Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "ProcessTracker.h"
+#include <QFile>
+#include <QDirIterator>
+#include <QDebug>
+
+#include <stdio.h>
+#include <signal.h>
+
+class ProcessTracker::Private
+{
+public:
+    Private(int processId)
+        : processId(processId)
+    {}
+
+    int processId;
+    QString commandLine;
+    QStringList arguments;
+    QFile logFile;
+};
+
+ProcessTracker::ProcessTracker(int processId, QObject* parent)
+    : QObject(parent)
+    , d(new Private(processId))
+{
+    QFile f(QString("/proc/%1/cmdline").arg(QString::number(processId)));
+    if(f.open(QIODevice::ReadOnly | QFile::Text)) {
+        QTextStream in(&f);
+        QString arg;
+        while(true) {
+            while(true) {
+                QChar chr = in.read(1).at(0);
+                if(chr.isNull()) {
+                    break;
+                }
+                arg.append(chr);
+            }
+            d->arguments.append(arg);
+            if(in.atEnd())
+                break;
+            arg.clear();
+        }
+        f.close();
+    }
+}
+
+ProcessTracker::~ProcessTracker()
+{
+    delete d;
+}
+
+QStringList ProcessTracker::arguments() const
+{
+    return d->arguments;
+}
+
+bool ProcessTracker::killProcess()
+{
+    int result = kill(d->processId, SIGTERM);
+    if(result == 0) {
+        return true;
+    }
+    return false;
+}
+
+QList< ProcessTracker* > ProcessTracker::getTrackers(QString executableFilePath, QObject* parent)
+{
+    QList<ProcessTracker*> trackers;
+
+    QDirIterator it("/proc", QDirIterator::NoIteratorFlags);
+    while (it.hasNext()) {
+        it.next();
+        if(it.fileName() == QString::number(it.fileName().toInt())) {
+            QFileInfo file(QString("%1/exe").arg(it.filePath()));
+            if(file.canonicalFilePath() == executableFilePath) {
+                trackers.append(new ProcessTracker(it.fileName().toInt(), parent));
+            }
+        }
+    }
+
+    return trackers;
+}
---
+++ src/controller/ProcessTracker.h
@@ -0,0 +1,64 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2015  Dan Leinir Turthra Jensen <admin@leinir.dk>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PROCESSTRACKER_H
+#define PROCESSTRACKER_H
+
+#include <QObject>
+#include <QStringList>
+
+class ProcessTracker : public QObject
+{
+    Q_OBJECT
+public:
+    ProcessTracker(int processId, QObject* parent = 0);
+    virtual ~ProcessTracker();
+
+    QStringList arguments() const;
+
+    /**
+     * Kill the process being tracked, and clean up logs for it.
+     *
+     * This function can also be used to clean up stale log entries by manually
+     * constructing a ProcessTracker for the PID in question and then calling
+     * this function.
+     * 
+     * @return Whether the cleanup operation was successful
+     */
+    bool killProcess();
+
+    /**
+     * Get an instance for each process which has a binary which matches
+     * the passed process name
+     * 
+     * @executableFilePath The name of the binary for which you want to track processes
+     * @parent       The QObject instance to parent the trackers to
+     * @return       A list of process trackers for the given process name. Can be empty.
+     */
+    static QList<ProcessTracker*> getTrackers(QString executableFilePath, QObject* parent);
+
+private:
+    class Private;
+    Private *d;
+};
+
+#endif // PROCESSTRACKER_H
