c5b2ddf00e0ae8624701826a6734814fa2ab9ad2
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-09-15 12:41:08 +0100

    Look for the right thing in Qt5...

--- cmake/FindQt5Core.cmake
+++ cmake/FindQt5Core.cmake
@@ -7,22 +7,24 @@ if (QT5CORE32_INCLUDE_DIRS AND QT5CORE32_LIBRARIES)
     set(QT5CORE32_FIND_QUIETLY TRUE)
 endif (QT5CORE32_INCLUDE_DIRS AND QT5CORE32_LIBRARIES)
 
-find_path(QT5CORE32_INCLUDE_DIR NAMES qglobal.h
+find_path(QT5CORE32_INCLUDE_DIR NAMES QtCoreVersion
     PATHS
     /usr/include/i386-linux-gnu/qt5/QtCore
+    ${CMAKE_INSTALL_PREFIX}/include/qt5/QtCore
     ${CMAKE_INSTALL_PREFIX}/include
     ${KDE4_INCLUDE_DIR}
+    /usr/include/qt5/QtCore
     /usr/include
     /usr/include/QtCore
-    /usr/include/qt5/QtCore
     )
-find_path(QT5CORE32_BASE_INCLUDE_DIR NAMES QtCore/qglobal.h
+find_path(QT5CORE32_BASE_INCLUDE_DIR NAMES QtCore/QtCoreVersion
     PATHS
     /usr/include/i386-linux-gnu/qt5
+    ${CMAKE_INSTALL_PREFIX}/include/qt5
     ${CMAKE_INSTALL_PREFIX}/include
     ${KDE4_INCLUDE_DIR}
-    /usr/include
     /usr/include/qt5
+    /usr/include
     )
 find_library(QT5CORE32_LIBRARIES NAMES Qt5Core libQt5Core PATHS /usr/lib /usr/lib/i386-linux-gnu/)
 SET(QT5CORE32_INCLUDE_DIRS "${QT5CORE32_INCLUDE_DIR};${QT5CORE32_BASE_INCLUDE_DIR}")
